e6ada3901ee2c097765a96296f6954716d1f24f1
Igor Shevchenko <igor.shevchenko.pro@gmail.com>
2020-04-17 21:35:17 +0300

    Create bar chart, directory structure, update styles

--- angular-dashboard-aspnet-core/src/app/app.module.ts
+++ angular-dashboard-aspnet-core/src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { SectionHealthComponent } from './sections/section-health/section-health.component';
 import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
 import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { BarChartComponent } from './charts/bar-chart/bar-chart.component';
+import { PieChartComponent } from './charts/pie-chart/pie-chart.component';
+import { LineChartComponent } from './charts/line-chart/line-chart.component';
 
 
 @NgModule({
@@ -18,10 +22,14 @@ import { SectionSalesComponent } from './sections/section-sales/section-sales.co
     SectionHealthComponent,
     SectionOrdersComponent,
     SectionSalesComponent,
+    BarChartComponent,
+    PieChartComponent,
+    LineChartComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
