b62c248ba020ad997e46c822948e02687686b153
Igor Shevchenko <igor.shevchenko.pro@gmail.com>
2020-04-17 19:32:25 +0300

    Setup routes

--- angular-dashboard-aspnet-core/src/app/app-routing.module.ts
+++ angular-dashboard-aspnet-core/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/sales', pathMatch: 'full' },
+  { path: 'sales', component: SectionSalesComponent },
+  { path: 'orders', component: SectionOrdersComponent },
+  { path: 'health', component: SectionHealthComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-dashboard-aspnet-core/src/app/app.module.ts
+++ angular-dashboard-aspnet-core/src/app/app.module.ts
@@ -5,12 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { SectionHealthComponent } from './sections/section-health/section-health.component';
+import { SectionOrdersComponent } from './sections/section-orders/section-orders.component';
+import { SectionSalesComponent } from './sections/section-sales/section-sales.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    SectionHealthComponent,
+    SectionOrdersComponent,
+    SectionSalesComponent,
   ],
   imports: [
     BrowserModule,
