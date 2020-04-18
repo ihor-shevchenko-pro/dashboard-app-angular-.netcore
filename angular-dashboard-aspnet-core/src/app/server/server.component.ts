fc466802e94877caa7ccfdfa0747538ba68f6c35
Igor Shevchenko <igor.shevchenko.pro@gmail.com>
2020-04-18 18:42:36 +0300

    Updated server component

--- angular-dashboard-aspnet-core/src/app/server/server.component.ts
+++ angular-dashboard-aspnet-core/src/app/server/server.component.ts
@@ -10,9 +10,37 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  public color: string;
+  public buttonText: string;
+  public serverStatus: string;
+  public isLoading: boolean;
+
   @Input() serverInput: Server;
 
   ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline);
+  }
+
+  private setServerStatus(isOnline: boolean) {
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.serverStatus = 'Online';
+      this.color = '#66BB6A',
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.serverStatus = 'Offline';
+      this.color = '#FF6B6B';
+      this.buttonText = 'Start';
+    }
+  }
+
+  sendServerAction(isOnline: boolean) {
+    console.log(this.serverInput.name, 'sendServerAction called!');
+    this.setServerStatus(!isOnline);
+    // this.makeLoading();
+    // const payload = this.buildPayload(isOnline);
+    // this.serverAction.emit(payload);
   }
 
 }
