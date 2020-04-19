61b8a4368779ce1f0a32c8c88e80abbddedfffd5
Igor Shevchenko <igor.shevchenko.pro@gmail.com>
2020-04-20 01:45:38 +0300

    Add server service

--- angular-dashboard-aspnet-core/src/app/sections/section-health/section-health.component.ts
+++ angular-dashboard-aspnet-core/src/app/sections/section-health/section-health.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Server } from 'src/app/core/models/server';
 
-const SAMPLE_SERVERS = [
-  { id: 1, name: 'dev-web', isOnline: true },
-  { id: 2, name: 'dev-mail', isOnline: false },
-  { id: 3, name: 'prod-web', isOnline: true },
-  { id: 4, name: 'prod-mail', isOnline: true }
-];
+// const SAMPLE_SERVERS = [
+//   { id: 1, name: 'dev-web', isOnline: true },
+//   { id: 2, name: 'dev-mail', isOnline: false },
+//   { id: 3, name: 'prod-web', isOnline: true },
+//   { id: 4, name: 'prod-mail', isOnline: true }
+// ];
 
 @Component({
   selector: 'app-section-health',
@@ -17,7 +17,9 @@ export class SectionHealthComponent implements OnInit {
 
   constructor() { }
 
-  public servers: Server[] = SAMPLE_SERVERS;
+  // public servers: Server[] = SAMPLE_SERVERS;
+
+  public servers: Server[];
   // timerSubscription: AnonymousSubscription;
 
   ngOnInit(): void {
---
+++ angular-dashboard-aspnet-core/src/app/services/server.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+import { Server } from '../core/models/server';
+import { HttpClient } from '@angular/common/http';
+import { RequestOptions, Response, Headers } from '@angular/http';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { ServerMessage } from '../core/models/server-message';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServerService {
+
+  constructor(private _http: HttpClient) {
+    this.headers = new Headers({
+      'Content-Type' : 'application/json',
+      'Accept' : 'q=0.8;application/json;q=0.9'
+    });
+
+    this.options = new RequestOptions({ headers: this.headers });
+   }
+
+   public options: RequestOptions;
+   public headers: Headers;
+ 
+   public getServers() {
+     return this._http.get<Server[]>('http://localhost:5000/api/server')
+     .pipe(map(res => res), catchError(this.handleError));
+   }
+ 
+   public handleServerMessage(msg: ServerMessage): Observable<Response> {
+     const url = 'http://localhost:5000/api/server/' + msg.id;
+     return this._http.put(url, msg, this.options)
+      .pipe(map(res => res));
+   }
+
+   private handleError(error: any) {
+    const errorMessage = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+
+    console.log(errorMessage);
+    return Observable.throw(errorMessage);
+  }
+}
